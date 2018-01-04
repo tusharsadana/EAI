1cb755bf1eb6586c33fe78f3bc4da0a4bb001d13
aesher9o1 <aashiskumar986@gmail.com>
2018-01-04 13:34:19 +0530

    Duniya Badal Di

---
+++ footer.php
@@ -0,0 +1,72 @@
+<?php
+?>
+
+
+  <!-- Footer -->
+  <footer class="footer">  
+            <div class="footer-widgets">   
+                <div class="container"style ="float:right">
+                    <div class="row" >
+                        <div class="col-md-3">  
+                            <div class="widget widget-text">
+                                <img src="images/index/manipal1.jpg" alt="image">
+                                <ul>
+                                    <li class="address">Dehmi Kalan, <br>Jaipur-Ajmer Expressway, Rajasthan</li>
+                                    <li class="phone"><a href="#">+91 - 95714 49301</a></li>
+                                    <li class="email"><a href="mailto:info@eaisociety.com">info@eaisociety.com</a></li>  
+                                </ul> 
+                            </div><!-- /.widget -->      
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="widget widget_tweets clearfix">
+                                <h5 class="widget-title">User Links</h5>
+                                <ul >
+                                    <li>
+                                        <a href="#">About Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Blog</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Contact</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Courses</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">FAQs</a>
+                                    </li>
+                                      <li>
+                                        <a href="#">Events</a>
+                                    </li>
+                                </ul>
+                               
+                            </div><!-- /.widget-recent-tweets -->
+                        </div><!-- /.col-md-2 -->
+
+                        
+                        <div class="col-md-3">
+                            <div class="widget widget-quick-contact">
+                                <h5 class="widget-title">Quick Contact</h5>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form class="flat-contact-form" id="contactform" method="post" action="http://themesflat.com/html/educate/contact/contact-process.php">
+
+                                            <input type="email" value="" tabindex="2" placeholder="Your Email" name="email" id="email-contact" required="">
+                                           
+                                            <textarea class="type-input" tabindex="3" placeholder="Message" name="message" id="message-contact" required=""></textarea> 
+                                            
+                                            <div class="submit-wrap">
+                                            <button class="flat-button bg-orange"><i class="fa fa-long-arrow-right"></i></button>
+                                            </div>
+                                           
+                                        </form><!-- /.comment-form -->     
+                                    </div><!-- /.col-md-12 -->
+                                </div>
+                            </div><!-- /.widget .widget-instagram -->
+                        </div>
+                    </div><!-- /.row -->
+                </div><!-- /.container -->
+            </div><!-- /.footer-widgets -->
+        </footer>
--- navbar.php
+++ navbar.php
@@ -1,43 +1,137 @@
-<?php 
+<?php
 ?>
 
+<div class="boxed">
+        <div class="windows8">
+            <div class="preload-inner">
+                <div class="wBall" id="wBall_1">
+                    <div class="wInnerBall"></div>
+                </div>
+                <div class="wBall" id="wBall_2">
+                    <div class="wInnerBall"></div>
+                </div>
+                <div class="wBall" id="wBall_3">
+                    <div class="wInnerBall"></div>
+                </div>
+                <div class="wBall" id="wBall_4">
+                    <div class="wInnerBall"></div>
+                </div>
+                <div class="wBall" id="wBall_5">
+                    <div class="wInnerBall"></div>
+                </div>
+            </div>
+        </div>
+    	<div class="header-inner-pages">
+    		<div class="top">
+    			<div class="container">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<div class="text-information">
+    							<p>Welcome to Exploration and Analysis of Information Society</p>
+    						</div>
+                            <div class="right-bar">
+        						<ul class="flat-information">
+        							<li class="phone">
+        								<a href="%2b61383766284.html" title="Phone number">+91 - 95714 49301</a>
+        							</li>
+        							<li class="email">
+        								<a href="mailto:info@eaisociety.com" title="Email address"> info@eaisociety.com</a>
+        							</li>
+                                </ul>
+                                <ul class="flat-socials">
+        							<li class="facebook">
+        								<a href="#">
+        									<i class="fa fa-facebook"></i>
+        								</a>
+        							</li>
+        							<li class="twitter">
+        								<a href="#">
+        									<i class="fa fa-twitter"></i>
+        								</a>
+        							</li>
+        							<li class="linkedin">
+        								<a href="#">
+        									<i class="fa fa-linkedin"></i>
+        								</a>
+        							</li>
+        							<li class="youtube">
+        								<a href="#">
+        									<i class="fa fa-youtube-play"></i>
+        								</a>
+        							</li>
+        						</ul>
+                            </div>
+    					</div>
+    				</div>
+    			</div>
+    		</div>      
+    	</div><!-- /.header-inner-pages -->
 
-<header class="navbar-fixed-top navbar animated-header dark" id="top-bar" style="box-shadow: 0 0 2em 0 rgba(0, 0, 0, 0.1);">
+    	<!-- Header --> 
+    	<header id="header" class="header clearfix"> 
+        	<div class="container">
+                <div class="header-wrap clearfix">
+                    <div id="logo" class="logo">
+                        <a href="index.html" rel="home">
+                            <img src="images/logo.png" alt="image">
+                        </a>
+                    </div><!-- /.logo -->            
+                    <div class="nav-wrap">
+                        <div class="btn-menu">
+                            <span></span>
+                        </div><!-- //mobile menu button -->
+                        <nav id="mainnav" class="mainnav">
+                            <ul class="menu"> 
+                                <li><a href ="index.php">Home</a></li>
+                                <li><a href ="index.php">Events</a></li>
+                                <li><a href="about-us.html">Membership</a></li>
+                                <li><a href="team.html">Members</a> </li>                               
+                                <li class="has-sub"><a href="courses-grid.html">About</a>
+                                	<ul class="submenu"> 
+                                        <li><a href="courses-grid.html">About 1</a></li>
+                                        <li><a href="courses-grid-sidebar.html">About 2</a></li> 
+                                    </ul>
+                                    </li> 
+                                <li><a href="contact.html">Contact</a>
+                                </li>
+                            </ul><!-- /.menu -->
+                        </nav><!-- /.mainnav -->    
+                    </div><!-- /.nav-wrap -->
 
-<div class="container">
 
-<div class="navbar-header"><!-- responsive nav button -->
-<button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
+                    <!-- Search
+                    <div id="s" class="show-search">
+                        <a href="#"><i class="fa fa-search"></i></a>         
+                    </div>  
+                    
+                    <div class="submenu top-search">
+                        <div class="widget widget_search">
+                            <form class="search-form">
+                                <input type="search" class="search-field" placeholder="Search …">
+                                <input type="submit" class="search-submit">
+                            </form>
+                        </div>
+                    </div>
+                </div> --> 
+            </div>
+        </header><!-- /.header -->
+
+
+
+
+
+
+
+        <!-- submenu
     
-</button><!-- /responsive nav button --><!-- logo -->
-
-<!-- /logo --></div>
-<!-- main menu -->
-
-<nav class="collapse navbar-collapse navbar-right" role="navigation">
-<div class="main-menu">
-<ul class="nav navbar-nav navbar-right">
-    <li><a href="index.php">Home</a></li>
-    <li><a href="events.php">Events</a></li>
-    <li><a href="membership.php">Membership</a></li>
-    <li><a href="members.php">Members</a></li>
-    <li><a href="contact.php">Contact</a></li>    
-</ul>
-</div>
-
-      
-    <div id="top-shadow"></div>
-    
-    </div>
-   
-</nav>
-<div class="logov" ><a href="index.php" ><img alt="" class="logoimage" src="img/logo.png"  /><br><br><br> </a></div>
-<!-- /main nav --></div>
-
-</header>
-
-
-
+     
+                                
+                                <li><a href="blog.html">About</a>
+                                    <ul class="submenu"> 
+                                        <li><a href="blog.html">About Data 1</a></li>
+                                        <li><a href="blog-single.html">About Data 2</a></li> 
+                                    </ul>
+                                </li>
+                                
+                                
+                                -->
