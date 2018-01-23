a5b55ede8f04df02c669d150dbb70c1c1cf2b0ed
aesher9o1 <aashiskumar986@gmail.com>
2018-01-23 10:02:01 +0530

    Network Calls Optimised

--- firebase/userAuth.js
+++ firebase/userAuth.js
@@ -1,46 +1,59 @@
-
-function user(){
-var x = document.getElementsByClassName("youtube")[0]
-var user = firebase.auth().currentUser;
-
-
+function user() {
+    var x = document.getElementsByClassName("youtube")[0]
+    var user = firebase.auth().currentUser;
+    var usernameCookie = getCookie("username");
 
 
-firebase.auth().onAuthStateChanged(function(user) {
 
-var LoggedIn ='<li class="youtube">Welcome!' +' ' + user.email +' '+ '<a href="" onclick="logout()" style ="margin-left:1em">Logout</a> </li>'
 
-    if (user) {
-     
+    firebase.auth().onAuthStateChanged(function(user) {
 
-         x.innerHTML = LoggedIn ;
-         var member = document.getElementById('membership');
-        member.style.display="none";
-        var course = document.getElementById('course');
-        course.style.display="inline-block";
-       
-    } else {
-    
 
-    }
 
-  });
+        if (user) {
+
+            var username;
+
+            if (usernameCookie != "") {
+
+                var LoggedIn = '<li class="youtube">Welcome!  ' + ' ' + Upper(usernameCookie) + ' ' + '<a href="" onclick="logout()" style ="margin-left:1em">Logout</a> </li>'
+                x.innerHTML = LoggedIn;
+            } else {
+                //get first name
+                firebase.database().ref('Students/' + user.uid).once('value').then(function(snapshot) {
+                    username = (snapshot.val() && snapshot.val().FirstName) || 'Anonymous';
+
+
+                    var LoggedIn = '<li class="youtube">Welcome!' + ' ' + Upper(username) + ' ' + '<a href="" onclick="logout()" style ="margin-left:1em">Logout</a> </li>'
+                    x.innerHTML = LoggedIn;
+                    var member = document.getElementById('membership');
+                    member.style.display = "none";
+                    var course = document.getElementById('course');
+                    course.style.display = "inline-block";
+                    document.cookie = "username=" + username + "; path=/";
+                });
+
+
+            }
+
+
+
+
+        } else {
+
+
+
+
+        }
+
+    });
 }
 
 
-function test(){
-    alert('hey');
-}
 
 
+function Signin() {
 
-
-
-
-
-
-function Signin(){
-    
     event.preventDefault();
     var userName = document.getElementById("userName").value;
     var middleName = document.getElementById("middleName").value;
@@ -53,135 +66,164 @@ function Signin(){
     var country = document.getElementById("country").value;
     var state = document.getElementById("state").value;
     var city = document.getElementById("city").value;
-    var message  = document.getElementById("message").value;         //street address
+    var message = document.getElementById("message").value; //street address
     var pinCode = document.getElementById("pinCode").value;
     var eMail = document.getElementById("eMail").value;
     var password = document.getElementById("password").value;
     var password2 = document.getElementById("password2").value;
     var noth = "0";
 
-   
-   
+
+
     //to delete
     var courses = {
 
-        MachineLearning : {
-            VideoWatched : 3,
-            PresentDone  : "47%"
+        MachineLearning: {
+            VideoWatched: 3,
+            PresentDone: "47%"
         },
 
     }
-    
-    
-    
+
+
+
     var rooms = {
-            
-            "1": {
-                Name : "Computer Method",
-                Validity : Date.now(),
-                purchasedAt: Date.now()
 
-            },
-            "2": {
-                Name : "Computer Method",
-                Validity : Date.now(),
-                PurchasedAt: Date.now()
+        "1": {
+            Name: "Computer Method",
+            Validity: Date.now(),
+            purchasedAt: Date.now()
 
-            }
+        },
+        "2": {
+            Name: "Computer Method",
+            Validity: Date.now(),
+            PurchasedAt: Date.now()
 
         }
-    
 
-  
-    if (password == password2){
-    
+    }
+
+
+
+    if (password == password2) {
+
         var details = {
-            Country :country,
-            State : state,
-            City : city,
-            Street : message,
-            Pin : pinCode
-           }
+            Country: country,
+            State: state,
+            City: city,
+            Street: message,
+            Pin: pinCode
+        }
 
-    
-    
+
+
         var data = {
-             FirstName :userName,
-             MiddleName:middleName, 
-             LastName : lastName ,
-             Interest :intrest, 
-             Adddress :details, 
-             PhoneNumber : phoneNumber,
-             Affiliation:affiliation,
-             Follows: courses,
-             DateOfBirth :dateOfBirth,
-             Gender : gender,
-             Subscription: rooms
+            FirstName: userName,
+            MiddleName: middleName,
+            LastName: lastName,
+            Interest: intrest,
+            Adddress: details,
+            PhoneNumber: phoneNumber,
+            Affiliation: affiliation,
+            Follows: courses,
+            DateOfBirth: dateOfBirth,
+            Gender: gender,
+            Subscription: rooms
+        }
+
+
+
+
+        firebase.auth().createUserWithEmailAndPassword(eMail, password)
+            .then(user => {
+
+                var user = firebase.auth().currentUser;
+                var ref = firebase.database().ref('Students/' + user.uid);
+                ref.set(data);
+                console.log(user);
+                location.href = "index.php";
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+
+
+
+        return true;
     }
-    
-   
-            var ref = firebase.database().ref('Students');
-            ref.push(data);
-    
 
-    firebase.auth().createUserWithEmailAndPassword(eMail,password)
-        .then(user =>{
+}
 
-            var user = firebase.auth().currentUser;
-            var ref = firebase.database().ref('Students/'+user.uid);
-            ref.set(data);
-            console.log(user);
-            location.href="index.php";
-        })
-        .catch(error =>{
-            console.log(error)
-        })
-
-    
-   
-
-   
-
-    return true;
-    }
-    
-    }
-    
-function login(){
+function login() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("pass").value;
     console.log(email);
     console.log(password);
 
     event.preventDefault();
-    firebase.auth().signInWithEmailAndPassword(email, password).then(user =>{
+    firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
 
-        var user = firebase.auth().currentUser;
-       
-        console.log(user);
-        
-        location.href="index.php";
-        user();
-        
-    })
-    
-    .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var er = document.getElementById('error');
-        er.innerHTML = 'Username or Password is not Correct.'
-      });
-    
+            var user = firebase.auth().currentUser;
 
-    }
+            console.log(user);
 
-function logout(){
+            location.href = "index.php";
+            user();
+
+        })
+
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var er = document.getElementById('error');
+            er.innerHTML = 'Username or Password is not Correct.'
+        });
+
+
+}
+
+
+
+function logout() {
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     firebase.auth().signOut().then(function() {
-        var logged = '<li class="youtube" id="youtube"> <p> <a href="#modal" id= "modal_trigger" >Login  </a> | <a href="signup.php" title="Register"> Register</a></p> </li>'
-        x.innerHTML =logged;
         // Sign-out successful.
-      }).catch(function(error) {
+        var logged = '<li class="youtube" id="youtube"> <p> <a href="#modal" id= "modal_trigger" >Login  </a> | <a href="signup.php" title="Register"> Register</a></p> </li>'
+        x.innerHTML = logged;
+
+    }).catch(function(error) {
         // An error happened.
-      });
+    });
+}
+
+
+
+
+//Utilities
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
+    return "";
+}
+
+function test() {
+    alert('hey');
+}
+
+function Upper(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
