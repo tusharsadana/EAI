d904a6072bca4d6c37cbbd495e6f2a9379f69155
aesher9o1 <aashiskumar986@gmail.com>
2018-02-24 22:22:05 +0530

    error

---
+++ error.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>IAESTE | Error</title>
+  <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+
+      <link rel="stylesheet" href="css/error.css">
+
+
+</head>
+
+<body>
+
+  <div class="container">
+  <div class="boo-wrapper">
+    <div class="boo">
+      <div class="face"></div>
+    </div>
+    <div class="shadow"></div>
+
+    <h1>Whoops!</h1>
+    <p>
+      We couldn't find the page you
+      <br />
+      were looking for.
+    </p>
+  </div>
+
+	<a href = "index.php"> <img src="images/logo.png" style=" margin-left: auto; margin-right: auto; display: block;"></a>
+
+</div>
+
+
+
+
+
+</body>
+
+
+</html>
